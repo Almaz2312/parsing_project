b7ee2d5a25504e0d53941c21bcf00c717fd541c0
Almaz2312 <Almazdurusaliev@gmail.com>
2022-07-04 14:50:06 +0600

    my first commit

---
+++ parse.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from datetime import datetime
+
+last_dt = f'last_dt={datetime.now()}'
+URL = f"http://newsline.kg/getNews.php?limit=5&{last_dt}"
+
+
+def collect_data():
+    response = requests.get(url=URL)
+    with open('news.json', 'w') as file:
+        json.dump(response.json()["data"], file, indent=4, ensure_ascii=False)
+
+
+def main():
+    collect_data()
+
+
+if __name__ == '__main__':
+    main()
