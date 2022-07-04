296e5b2a7c65bc28ac817dc72e1b89b5aff35986
Almaz2312 <Almazdurusaliev@gmail.com>
2022-07-04 16:52:17 +0600

    удалил ненужные установки с requirements.txt, добавил комментарии и написал README

--- parse.py
+++ parse.py
@@ -2,11 +2,16 @@ import requests
 import json
 from datetime import datetime
 
+# Добавил код last_dt, чтобы дата была текущей
 last_dt = f'last_dt={datetime.now()}'
 URL = f"http://newsline.kg/getNews.php?limit=5&{last_dt}"
 
 
 def collect_data():
+    """
+    Отправляет запрос для стягивания данных с сайта (адрес прописан в URL) и сохранияет
+    данные в news.json файл.
+    """
     response = requests.get(url=URL)
     with open('news.json', 'w', encoding='utf-8') as file:
         json.dump(response.json()["data"], file, indent=4, ensure_ascii=False)
